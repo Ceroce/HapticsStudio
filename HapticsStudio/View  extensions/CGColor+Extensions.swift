b229825c66a65ef411e0fa924525b8e704a7a0ec
Renaud Pradenc <rpradenc@incwo.com>
2021-11-02 21:04:04 +0100

    Small reorganization of the projet files.

--- HapticsStudio/View  extensions/CGColor+Extensions.swift
+++ HapticsStudio/View  extensions/CGColor+Extensions.swift
@@ -1,25 +1,12 @@
 //
-//  Color+Extensions.swift
+//  CGColor+Extensions.swift
 //  HapticsStudio
 //
-//  Created by Renaud Pradenc on 01/11/2021.
+//  Created by Renaud Pradenc on 02/11/2021.
 //
 
 import Foundation
 import CoreGraphics
-import SwiftUI
-
-/// Linear interpolation between two Color's.
-///
-/// THIS METHOD DOES NOT WORK WITH ASSETS BECAUSE YOU CANNOT GET THE CGCOLOR FOR A SO-CALLED DYNAMIC COLOR.
-func mix(_ minColor: Color, _ maxColor: Color, ratio: CGFloat) -> Color {
-    guard let leftCGColor = minColor.cgColor,
-          let rightCGColor = maxColor.cgColor else {
-              NSLog("Could not get CGColors from SwiftUI Colors")
-              return minColor
-          }
-    return Color(mix(leftCGColor, rightCGColor, ratio: ratio))
-}
 
 /// Linear interpolation between two CGColor's.
 func mix(_ minColor: CGColor, _ maxColor: CGColor, ratio: CGFloat) -> CGColor {
